cd59e523c4ed486c7d33f7866b52b4e2191e2605
Aldair Andrade <demianmaster2003@gmail.com>
2026-02-20 11:05:44 -0500

    feat: Creando formulario Signup

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 
 # Create your views here.
 def helloworld(request):
-    return render(request, "signup.html")
+  title = "hello world"
+  return render(request, "signup.html",{
+    "form": UserCreationForm()}
+  )
+
+def bye(request):
+  return render(request, "bye.html")
