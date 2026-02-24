a783b20ec9d5b44ad7f4e75e9fffdafcb2afefec
Aldair Andrade <demianmaster2003@gmail.com>
2026-02-24 15:16:30 -0500

    feat completar y eliminar tareas

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 # Create your views here.
 def home(request):
     return render(request, "home.html")
@@ -55,8 +56,33 @@ def create_task(request):
             })
 
 def task_detail(request,task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    return render(request, 'task_detail.html', {'task': task})
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            TaskForm(request.POST, instance=Task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html',{
+                'task': task, 'form': form, 'error': "Error updating task"
+            })
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
 
 def signout(request):
     logout(request)
