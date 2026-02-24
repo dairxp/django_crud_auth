a30d0fcc68c71b7067ea07b4111b01c97b67345a
Aldair Andrade <demianmaster2003@gmail.com>
2026-02-24 15:44:02 -0500

    feat: proteger rutas @login_required de [django.contrib.auth.decorators import login_required]

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,8 @@ from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def home(request):
     return render(request, "home.html")
@@ -32,14 +34,17 @@ def signup(request):
             'error': 'Password do not match'
         })
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {'tasks': tasks})
 
+@login_required
 def tasks_complete(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'tasks.html', {'tasks': tasks})
 
+@login_required
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html', {
@@ -59,6 +64,7 @@ def create_task(request):
                 'error' : 'Please provide valide data'
             })
 
+@login_required
 def task_detail(request,task_id):
     if request.method == 'GET':
         task = get_object_or_404(Task, pk=task_id)
@@ -75,6 +81,7 @@ def task_detail(request,task_id):
                 'task': task, 'form': form, 'error': "Error updating task"
             })
 
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
@@ -82,12 +89,14 @@ def complete_task(request, task_id):
         task.save()
         return redirect('tasks')
 
+@login_required
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.delete()
         return redirect('tasks')
 
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
